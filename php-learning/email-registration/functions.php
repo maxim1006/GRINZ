9b7e3f7d90873a48d66d6efffd23a7bd1c7f5f81
Max <maxim1006@list.ru>
2015-07-13 12:39:48 +0300

    added registered users lesson

--- php-learning/email-registration/functions.php
+++ php-learning/email-registration/functions.php
@@ -1,10 +1,23 @@
 <?php
+    //выношу переменные
+    //echo $_SERVER['DOCUMENT_ROOT']; - //так узнаю корень документа
+    define('MAILING_LIST', 'admin/mailing_list.php');
 
+
+
+    /*
+     * Add a new item to the registered users list
+     */
     function add_registered_user($name, $email) {
-        file_put_contents('mailing_list.php', "$name: $email\n", FILE_APPEND); //для защиты этого файла, его стоит расположить выше document root ../
+        file_put_contents(MAILING_LIST, "$name: $email\n", FILE_APPEND); //для защиты этого файла, его стоит расположить выше document root ../
     }
 
-//нужна для того, чтобы имя или емейл не пропадал при отправке запроса
+
+
+    //нужна для того, чтобы имя или емейл не пропадал при отправке запроса
+    /*
+    * Save input information
+    */
     function old($key) {
         if (!empty($_REQUEST[$key])) {
             return htmlspecialchars($_REQUEST[$key]);
@@ -13,9 +26,33 @@
         return '';
     }
 
+
+    /**
+     * Validate email
+     * @param $email
+     * @return mixed
+     */
     function valid_email($email) {
         //return preg_match('/regular expression/', $email);
         return filter_var($email, FILTER_VALIDATE_EMAIL); //встроенная в php функция, проверяет email и возвращает true/false
+    }
+
+
+    /**
+     * Get registered users
+     * @param string $path
+     * @return array|bool
+     */
+    function get_registered_users($path = MAILING_LIST) { //первым параметром ф-ции список из email адерсов
+        $users = file($path); //прочтет файл по пути и вернет массив в котором каждая строка - элемент массива
+
+        if (count($users)) {
+            return array_map(function($user) {
+                return explode(': ', htmlspecialchars($user)); //разбиваем массив по : и пробелу, типо как split
+            }, $users);
+        }
+
+        return false;
     }
 
 ?>
